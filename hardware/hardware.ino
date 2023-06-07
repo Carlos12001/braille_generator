77036f7cfa14ad7f0d0ba14319577fdf2a5c961b
Carlos12001 <carlos.andres12001@gmail.com>
2023-06-06 19:12:21 -0600

    feat: almost braile finish

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -8,22 +8,46 @@ Adafruit_PWMServoDriver myServo = Adafruit_PWMServoDriver();
 #define SERVOMAX 300
 #define NUMSERVOS 12
 
+int changeSomething = 1;
+const int errorLed = 5;
+String receivedString = "";
+
 void setup() {
   Serial.begin(9600);
   myServo.begin();
   myServo.setPWMFreq(60);
+  pinMode(errorLed, OUTPUT);
   delay(10);
   parseAndMoveServos("000000000000a");
+  digitalWrite(errorLed,LOW);
+  Serial.println("listo"); 
 }
 
 void loop() {
-  delay(2000);
-  parseAndMoveServos("100000000000a");
-  delay(2000);
-  parseAndMoveServos("010001000000a");
-  delay(2000);
-  parseAndMoveServos("000000000000a");
-  delay(2000);
+  while (Serial.available()) {  
+    delay(10);  
+    char c = Serial.read(); 
+    receivedString += c;
+  }
+
+  if (receivedString.length()!=0) {
+    if (isValidInput(receivedString)) {
+      parseAndMoveServos(receivedString);
+      changeSomething = 1;
+      digitalWrite(errorLed,LOW);
+      //imprima(receivedString)
+    }
+    else{
+      parseAndMoveServos("000000000000a");
+      digitalWrite(errorLed,HIGH);
+    }
+    receivedString = "";
+  }
+
+  if(changeSomething){
+    changeSomething = 0;
+    Serial.println("Hi Python!");
+  }
 }
 
 void parseAndMoveServos(String input) {
@@ -33,8 +57,21 @@ void parseAndMoveServos(String input) {
     myServo.setPWM(i, 0, targetPos); 
     delay(DELAYSERVO);
   }
-
-  // Imprimir el último caracter
-  Serial.println(input.charAt(input.length()-1));
 }
+
+bool isValidInput(String input) {
+  if (input.length() != NUMSERVOS + 1) {
+    return false;
+  }
+
+  for (int i = 0; i < NUMSERVOS; i++) {
+    char c = input.charAt(i);
+    if (c != '0' && c != '1') {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 
